cc490a4c6498133a6598469a688ee05962c72127
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-09-30 17:45:58 -0500

    commit tree 2fcfcd7af1e8617a78fd0c27fbc0ef641dc5e5d2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+//
+
+
+
+function superbowlWin (records){
+    const whatever=records.find(record => record.result === "W");
+    return whatever? whatever.year: undefined
+}
+console.log (record.find (superbowlWin));
